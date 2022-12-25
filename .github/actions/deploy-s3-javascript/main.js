dfa06f0613b0856b9b5920945d0a2b662202b59c
Nikhil <nikhilbanuri4@gmail.com>
2022-12-25 18:55:49 +0530

    url is corrected

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
@@ -14,7 +13,7 @@ function run() {
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
 
-  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+  const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`
   core.setOutput('website-url', websiteUrl);
 }
 
